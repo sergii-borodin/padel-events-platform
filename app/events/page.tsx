415ce34cdf6d2842a2182318848bb9144580a1be
Sergii Borodin <sergii.borodin@yahoo.com>
2026-07-23 21:45:48 +0200

    Enhance event display logic in Home component
    - Added conditional rendering to display a message when no events are available, improving user experience by informing users about the lack of events.
    - Maintained the existing event listing structure for when events are present, ensuring consistent functionality.

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -14,26 +14,33 @@ const Home = async () => {
       <CreateEventBtn />
       <div className="mt-20 space-y-7">
         <h3>Explore and book a game you like</h3>
-        <ul className="events">
-          {events.map((event: IEvent) => (
-            <li key={event.slug}>
-              <EventCard
-                title={event.title}
-                image={event.image}
-                slug={event.slug}
-                location={event.location}
-                date={event.date}
-                time={event.time}
-                venueType={event.venueType}
-                minRating={event.minRating}
-                maxRating={event.maxRating}
-                bookingsCount={event.bookingsCount ?? 0}
-                maxParticipants={event.maxParticipants}
-                duration={event.duration}
-              />
-            </li>
-          ))}
-        </ul>
+        {events.length === 0 ? (
+          <p>
+            Unfortunately, no events to join at the moment. Please, check later
+            for updates. Remember you can always create your own event here.
+          </p>
+        ) : (
+          <ul className="events">
+            {events.map((event: IEvent) => (
+              <li key={event.slug}>
+                <EventCard
+                  title={event.title}
+                  image={event.image}
+                  slug={event.slug}
+                  location={event.location}
+                  date={event.date}
+                  time={event.time}
+                  venueType={event.venueType}
+                  minRating={event.minRating}
+                  maxRating={event.maxRating}
+                  bookingsCount={event.bookingsCount ?? 0}
+                  maxParticipants={event.maxParticipants}
+                  duration={event.duration}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </section>
   );
